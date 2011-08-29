4f12f06b14866e002140ffb804ea8177ca1dea8b
Nebojša Jakovljević <nebjak@gmail.com>
2011-08-29 06:32:27 +0200

    v1.0 - DEA u pripremi

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset="UTF-8" />
+        <meta name="description" content="Energoglobal doo, Kneza Milosa 33, 21000 Novi Sad, Srbija" />
+        <meta name="keywords" content="mini-hidroelektrana, struja, energetika, elektrana, mini, hidroelektrana, dizlel, dizel-agregat, agregat, dea, vodenica" />
+        <meta name="author" content="Nebojsa Jakovljevic // nebojsa.jakovlejvic [at] energoglobal.com" />
         <link rel="author" href="humans.txt" />
-        <title></title>
+        <title>Energoglobal doo - Dizel agregati - Mini hidroelektrane</title>
         <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
         <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
         <link rel="stylesheet" type="text/css" media="all" href="css/960.css" />
@@ -21,11 +24,16 @@
             <div class="clear"></div>
             <div class="prefix_1 grid_5 tile">
                 <p>
-                    <a href="#">
+                    <a href="http://www.minihidroelektrane.com/">
                         <img src="img/mhe.jpg" alt="Mini hidro elektrane" title="Mini hidro elektrane" />
-                        <strong>Mini hidro elektrane</strong><br/>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        <strong>Mini hidroelektrane</strong><br/>
+                        Energoglobal vrši detaljnu analizu troškova projektovanja, 
+                        izgradnje i nadzora, kao i dobiti mini-hidroelektrana (MHE). 
+                        Na bazi analize Energoglobal predlaže optimalne parametre energetskog objekta, 
+                        što u slučaju MHE znači tip turbine, optimalni instalisani protok, 
+                        optimalni bruto pad i optimalnu nominalnu snagu. Drugim rečima, 
+                        Energoglobal za svoje partnere priprema kompletno idejno rešenje 
+                        energetskog objekta, projekat, kao i izvođenje radova i nadzora.
                     </a>
                 </p>
             </div>
@@ -34,10 +42,9 @@
                     <a href="#" style="float: right;">
                         <img src="img/dea.jpg" alt="Dizel elektricni agregati" title="Dizel elektricni agregati" />
                         <strong>Dizel elektricni agegati</strong><br/>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        <br/>
+                        <br/>
+                        Odeljak u pripremi, uskoro više!
                     </a>
                 </p>
             </div>
