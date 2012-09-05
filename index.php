54ec3556a268ea916b0215b97a4795ccbce72cb6
Nebojša Jakovljević <nebjak@gmail.com>
2012-09-05 09:38:04 +0200

    promenjen tabstop 4 -> 2

--- index.php
+++ index.php
@@ -1,73 +1,73 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="description" content="Energoglobal doo, Kneza Milosa 33, 
-              21000 Novi Sad, Srbija - Konsalting, projektovanje i izvodjenje - 
-              Obnovljivi izvori energije - male hidroelektrane, biomasa, biogas, 
-              ... - Pomoćni izvori napajanja - dizel električni agregati, UPS" />
-        <meta name="keywords" content="mini-hidroelektrana, struja, energetika, 
-              elektrana, mini, hidroelektrana, dizlel, dizel-agregat, agregat, 
-              dea, vodenica, ups, konsalting, projektovanje, biomasa, biogas, 
-              napajanje, turbina, generator, izvodjenje" />
-        <meta name="author" content="Nebojsa Jakovljevic // nebojsa.jakovlejvic [at] energoglobal.com" />
-        <link rel="author" href="humans.txt" />
-        <title>Energoglobal doo - Dizel agregati - Mini hidroelektrane</title>
-        <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/960.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/main.css" />
-        <script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
-        <script type="text/javascript" src="js/main.js"></script>
-    </head>
-    <body>
-        <div class="container_12">
-            <div class="grid_12">
-                <p>
-                    <img src="img/logo-940.png" alt="Energoglobal d.o.o. - Novi Sad" title="Energoglobal d.o.o. - Novi Sad" />
-                </p>
-            </div>
-            <div class="clear"></div>
-            <div class="prefix_1 grid_5 tile">
-                <p>
-                    <a href="http://www.minihidroelektrane.com/">
-                        <img src="img/mhe.jpg" alt="Mini hidro elektrane" title="Mini hidro elektrane" />
-                        <strong>Mini hidroelektrane</strong><br/>
-                        Energoglobal vrši detaljnu analizu troškova projektovanja, 
-                        izgradnje i nadzora, kao i dobiti mini-hidroelektrana (MHE). 
-                        Na bazi analize Energoglobal predlaže optimalne parametre energetskog objekta, 
-                        što u slučaju MHE znači tip turbine, optimalni instalisani protok, 
-                        optimalni bruto pad i optimalnu nominalnu snagu. Drugim rečima, 
-                        Energoglobal za svoje partnere priprema kompletno idejno rešenje 
-                        energetskog objekta, projekat, kao i izvođenje radova i nadzora.
-                    </a>
-                </p>
-            </div>
-            <div class="grid_5 suffix_1 tile">
-                <p>
-                    <a href="http://www.dizelagregat.com/" style="float: right;">
-                        <img src="img/dea.jpg" alt="Dizel elektricni agregati" title="Dizel elektricni agregati" />
-                        <strong>Dizel električni agegati</strong><br/>
-                        Energoglobal doo vrši prodaju novih, kao i generalno 
-                        repariranih polovnih dizel električnih agregata. 
-                        Pored prodaje vršimo i servis što obuhvata hitne 
-                        intervencije i redovno održavanje. Posedujemo flotu od 
-                        više agregata različitih snaga i vršimo usluge rentiranja 
-                        sa mogućnošću instalacije, prevoza na objekat i stalnog 
-                        prisustva stručnog lica.
-                    </a>
-                </p>
-            </div>
-            <div class="clear"></div>
-            <div class="grid_12" id="footer">
-                <p>
-                    <strong>ENERGOGLOBAL D.O.O.</strong>, Somborska rampa 25, 21000 Novi Sad&nbsp;
-                    <strong>Tel:</strong> +381 21 499 315&nbsp;<strong>Fax:</strong> +381 21 499 315&nbsp;
-                    <strong>E-mail:</strong> office@energoglobal.com
-                    <br/>
-                    &copy; 2011. www.energoglobal.com
-                </p>
-            </div>
-        </div>
-    </body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Energoglobal doo, Somborska rampa 25,
+    21000 Novi Sad, Srbija - Konsalting, projektovanje i izvodjenje -
+    Obnovljivi izvori energije - male hidroelektrane, biomasa, biogas,
+    ... - Pomoćni izvori napajanja - dizel električni agregati, UPS" />
+    <meta name="keywords" content="mini-hidroelektrana, struja, energetika,
+    elektrana, mini, hidroelektrana, dizlel, dizel-agregat, agregat,
+    dea, vodenica, ups, konsalting, projektovanje, biomasa, biogas,
+    napajanje, turbina, generator, izvodjenje" />
+    <meta name="author" content="Nebojsa Jakovljevic // nebojsa.jakovlejvic [at] energoglobal.com" />
+    <link rel="author" href="humans.txt" />
+    <title>Energoglobal doo - Dizel agregati - Mini hidroelektrane</title>
+    <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="css/960.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="css/main.css" />
+    <script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+  </head>
+  <body>
+    <div class="container_12">
+      <div class="grid_12">
+        <p>
+          <img src="img/logo-940.png" alt="Energoglobal d.o.o. - Novi Sad" title="Energoglobal d.o.o. - Novi Sad" />
+        </p>
+      </div>
+      <div class="clear"></div>
+      <div class="prefix_1 grid_5 tile">
+        <p>
+          <a href="http://www.minihidroelektrane.com/">
+            <img src="img/mhe.jpg" alt="Mini hidro elektrane" title="Mini hidro elektrane" />
+            <strong>Mini hidroelektrane</strong><br/>
+            Energoglobal vrši detaljnu analizu troškova projektovanja,
+            izgradnje i nadzora, kao i dobiti mini-hidroelektrana (MHE).
+            Na bazi analize Energoglobal predlaže optimalne parametre energetskog objekta,
+            što u slučaju MHE znači tip turbine, optimalni instalisani protok,
+            optimalni bruto pad i optimalnu nominalnu snagu. Drugim rečima,
+            Energoglobal za svoje partnere priprema kompletno idejno rešenje
+            energetskog objekta, projekat, kao i izvođenje radova i nadzora.
+          </a>
+        </p>
+      </div>
+      <div class="grid_5 suffix_1 tile">
+        <p>
+          <a href="http://www.dizelagregat.com/" style="float: right;">
+            <img src="img/dea.jpg" alt="Dizel elektricni agregati" title="Dizel elektricni agregati" />
+            <strong>Dizel električni agegati</strong><br/>
+            Energoglobal doo vrši prodaju novih, kao i generalno
+            repariranih polovnih dizel električnih agregata.
+            Pored prodaje vršimo i servis što obuhvata hitne
+            intervencije i redovno održavanje. Posedujemo flotu od
+            više agregata različitih snaga i vršimo usluge rentiranja
+            sa mogućnošću instalacije, prevoza na objekat i stalnog
+            prisustva stručnog lica.
+          </a>
+        </p>
+      </div>
+      <div class="clear"></div>
+      <div class="grid_12" id="footer">
+        <p>
+          <strong>ENERGOGLOBAL D.O.O.</strong>, Somborska rampa 25, 21000 Novi Sad&nbsp;
+          <strong>Tel:</strong> +381 21 499 315&nbsp;<strong>Fax:</strong> +381 21 499 315&nbsp;
+          <strong>E-mail:</strong> office@energoglobal.com
+          <br/>
+          &copy; 2011. www.energoglobal.com
+        </p>
+      </div>
+    </div>
+  </body>
 </html>
