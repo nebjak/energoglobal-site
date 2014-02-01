c45bea045d97cb1d9ce939e24cb7984873f79420
Nebojša Jakovljević <nebjak@gmail.com>
2014-02-01 07:58:33 +0100

    Changed copyright year to 2014. fixes #2

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
           <a href="https://twitter.com/energoglobal" class="twitter-follow-button" data-show-count="false">Follow @energoglobal</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
           <br>
-          &copy; 2013. www.energoglobal.com
+          &copy; 2014. www.energoglobal.com
         </p>
       </div>
     </div>
